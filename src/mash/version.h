3bb57ca8ce814ea1f19f6d0bf752a650eec1b6d9
Chirag Jain <cjain7@gatech.edu>
2017-02-17 15:28:13 -0500

    Removed capnp as dependency

--- src/mash/version.h
+++
@@ -1,7 +0,0 @@
-// Copyright © 2015, Battelle National Biodefense Institute (BNBI);
-// all rights reserved. Authored by: Brian Ondov, Todd Treangen,
-// Sergey Koren, and Adam Phillippy
-//
-// See the LICENSE.txt file included with this software for license information.
-
-static const char * version = "1.1.1";
