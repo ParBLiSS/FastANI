8ac4ff84560d230e1a2b5a8b3bd1d3029cb3a9f3
Chirag Jain <cjain7@gatech.edu>
2021-05-02 16:03:45 +0530

    Update commonFunc.hpp

--- src/map/include/commonFunc.hpp
+++ src/map/include/commonFunc.hpp
@@ -143,9 +143,9 @@ namespace skch
               Q.pop_back();
 
             //Push currentKmer and position to back of the queue
-            //0 indicates the dummy window # (will be updated later)
+            //-1 indicates the dummy window # (will be updated later)
             Q.push_back( std::make_pair(
-                  MinimizerInfo{currentKmer, seqCounter, 0},
+                  MinimizerInfo{currentKmer, seqCounter, -1},
                   i)); 
 
             //Select the minimizer from Q and put into index
