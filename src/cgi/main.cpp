11106466b1f27dd61cfc493e45444aafd5594cce
Sriram P C <sriram.pc@gmail.com>
2023-01-08 11:38:55 -0500

    Added Github workflow for CI :Build/Test with CMake/CTest and code coverage with codecov.

--- src/cgi/main.cpp
+++ src/cgi/main.cpp
@@ -2,7 +2,7 @@
 // Created by srirampc on 11/30/22.
 //
 
-int cgi_main(int argc, const char **argv);
+int cgi_main(int argc, char **argv);
 
 int main(int argc, char** argv) {
     return cgi_main(argc, argv);
